0e0a972be1f04c2c8d48e014d998c3c878d6f78c
Urja Acharya <urja.acharya@huskers.unl.edu>
2020-04-09 23:22:45 -0700

    added struct to parse json file input

--- src/helpers.go
+++ src/helpers.go
@@ -19,15 +19,98 @@ func Usage() {
 	os.Exit(1)
 }
 
-//ReadArgs Reads user provided arguments
-func ReadArgs() (string, string, interface{}, interface{}, map[string]interface{}, string) {
-	inputArgsFile := flag.String("inputArgs", "", "REQUIRED: input arguments")
+func verifyArguments() {
+	inputArgsFile := flag.String("inputArgsFile", "", "REQUIRED: input json with input arguments")
 	flag.Usage = Usage
 	flag.Parse()
 	file, err := ioutil.ReadFile(*inputArgsFile)
 	var args map[string]interface{}
 	err = json.Unmarshal(file, &args)
 
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+	fmt.Println(args["new"])
+	if args["input_image"] == nil {
+		log.Fatalf("input_image missing in input json")
+	}
+	if args["output"] == nil {
+		log.Fatalf("Path to output directory missing in input json")
+	}
+	if args["output"] == nil {
+		log.Fatalf("Path to output directory missing in input json")
+	}
+}
+
+type arguments struct {
+	Input_image      string
+	Output           output
+	Link             link
+	Site_webmanifest webmanifest
+	Icons            icons
+}
+type output struct {
+	Images_path string
+	HTML        html
+}
+
+type html struct {
+	Path string
+	Name string
+}
+type link struct {
+	Href_prefix string
+	Href_suffix string
+}
+type webmanifest struct {
+	Background_color string
+	Name             string
+	Short_name       string
+	Theme_color      string
+}
+
+type icons struct {
+	Favicon16  faviconInfo `json:"favicon-16"`
+	Favicon32  faviconInfo `json:"favicon-32"`
+	Favicon120 faviconInfo `json:"favicon-120"`
+	Favicon128 faviconInfo `json:"favicon-128"`
+	Favicon152 faviconInfo `json:"favicon-152"`
+	Favicon167 faviconInfo `json:"favicon-167"`
+	Favicon180 faviconInfo `json:"favicon-180"`
+	Favicon192 faviconInfo `json:"favicon-192"`
+	Favicon196 faviconInfo `json:"favicon-196"`
+}
+
+type faviconInfo struct {
+	Width  uint16
+	Height uint16
+	Rel    []string
+}
+
+func test() {
+	new_file, err := ioutil.ReadFile("./test.json")
+	var names arguments
+	err = json.Unmarshal(new_file, &names)
+	fmt.Println(names)
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+}
+
+//ReadArgs Reads user provided arguments
+func ReadArgs() (string, string, interface{}, interface{}, map[string]interface{}, string) {
+	test()
+	inputArgsFile := flag.String("inputArgs", "", "REQUIRED: input arguments")
+	flag.Usage = Usage
+	flag.Parse()
+	var names arguments
+
+	file, err := ioutil.ReadFile(*inputArgsFile)
+	var args map[string]interface{}
+	err = json.Unmarshal(file, &args)
+	err = json.Unmarshal(file, &names)
+	fmt.Println(names)
+	// To do: add function to check input arguments in input json
 	if err != nil {
 		log.Fatalf("ERROR: %v", err)
 	}
